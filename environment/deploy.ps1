85f2c0da2daeebfce441853d0f8e4d1676bdf054
Adam Marczak <adam.marczak@gmail.com>
2019-09-09 22:39:57 +0200

    Initial Test

---
+++ environment/deploy.ps1
@@ -0,0 +1,9 @@
+$subscriptionId = "84717e9e-5b58-4068-a903-2604a799282c"
+$resourceGroup = "demo"
+
+Select-AzSubscription -SubscriptionId $subscriptionId
+
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $resourceGroup `
+    -TemplateParameterFile functions-and-keyvault.parameters.json `
+    -TemplateFile functions-and-keyvault.json 
