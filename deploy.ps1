d428263d1fbe67549c09c4350cf94b44c0997ea4
Adam Marczak <adam.marczak@gmail.com>
2019-09-24 21:43:34 +0200

    removed deploy script

--- deploy.ps1
+++
@@ -1,9 +0,0 @@
-$subscriptionId = ""
-$resourceGroup = ""
-
-Select-AzSubscription -SubscriptionId $subscriptionId
-
-New-AzResourceGroupDeployment `
-    -ResourceGroupName $resourceGroup `
-    -TemplateParameterFile logic-app-refresh.parameters.json `
-    -TemplateFile logic-app-refresh.json 
